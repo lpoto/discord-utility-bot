5f33c89ea5fdefd88472dfcc92b9e75a8c5e2d23
potocnikluka <luka.potochnik@gmail.com>
2022-02-10 19:19:16 +0100

    Add skip button

--- src/music/commands/skip.ts
+++ src/music/commands/skip.ts
@@ -1,3 +1,19 @@
+import { MessageButton } from 'discord.js';
+import { MessageButtonStyles } from 'discord.js/typings/enums';
+import { MusicCommandOptions } from '.';
 import { Command } from './command';
 
-export class Skip extends Command {}
+export class Skip extends Command {
+    constructor(options: MusicCommandOptions) {
+        super(options);
+    }
+
+    get button(): MessageButton {
+        return new MessageButton()
+            .setLabel(
+                this.translate(['music', 'commands', 'actionRow', 'skip']),
+            )
+            .setStyle(MessageButtonStyles.SECONDARY)
+            .setCustomId(this.id);
+    }
+}
